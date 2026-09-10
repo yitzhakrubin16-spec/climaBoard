5e109392a6e392abb794b90e8ed7788dfe1293c5
Yitzhak Rubin <yitzhakrubin16@gmail.com>
2026-09-10 13:21:45 +0300

    Add favorite deletion endpoint

--- backend/routers/favorites_routes.py
+++ backend/routers/favorites_routes.py
@@ -1,7 +1,7 @@
 from typing import Annotated
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, HTTPException
 from schemas.favorites import FavoriteCreate, UserNameSearchParams
-from services.favorite_service import add_favorite_service, get_favorites_service
+from services.favorite_service import add_favorite_service, get_favorites_service, delete_favorite_service
 
 router = APIRouter(prefix="/favorites")
 
@@ -12,3 +12,12 @@ def add_favorite(favorite: FavoriteCreate):
 @router.get("/")
 def get_favorites(params: Annotated[UserNameSearchParams, Query()]):
     return get_favorites_service(user_name=params.user_name)
+
+@router.delete("/{city_id}")
+def delete_favorite(params: Annotated[UserNameSearchParams, Query()], city_id: int):
+    res = delete_favorite_service(user_name=params.user_name, city_id=city_id)
+
+    if res is None:
+        raise HTTPException(status_code=404, detail="favorite not found")
+    else:
+        return res
--- backend/services/favorite_service.py
+++ backend/services/favorite_service.py
@@ -9,8 +9,16 @@ def add_favorite_service(favorite: FavoriteCreate):
 def get_favorites_service(user_name: str):
     user_favorites = []
 
-    for i in range(len(favorites)):
-        if favorites[i].user_name == user_name:
-            user_favorites.append(favorites[i])
+    for favorite in favorites:
+        if favorite.user_name == user_name:
+            user_favorites.append(favorite)
 
     return user_favorites        
+
+def delete_favorite_service(user_name: str, city_id: int):
+
+    for i in range(len(favorites)):
+        if favorites[i].user_name == user_name and favorites[i].city_id == city_id:
+            return favorites.pop(i)
+
+    return None                 
